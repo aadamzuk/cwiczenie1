6aaf846aebd42d33c2aa4c8b84e6d78ba446281f
aadamzuk <32269564+aadamzuk@users.noreply.github.com>
2017-09-25 16:47:24 +0200

    zmiany

--- cw1/main.cpp
+++ cw1/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string imie="Adam",nazwisko="Zuk";
+    int rok_urodzenia=1997,aktualny_rok=2017;
+    cout<<"Witam, nazywam sie "<<imie<<" "<<nazwisko<<" i mam "<<aktualny_rok-rok_urodzenia<<" lat"<<endl;
+
     return 0;
 }
